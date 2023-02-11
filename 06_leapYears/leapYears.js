b77193d2ab2fa68ad929348d96deb17863a5d8b7
jdittert <jondittert@gmail.com>
2023-02-11 10:47:50 -0500

    Solve through exercise 07

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 400 === 0) return true;
+    if (year % 4 === 0 && year % 100 !== 0) {        
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// return year % 4 === 0 && ( year % 100 !== 0 || year % 400 === 0);
+// Don't forget === will auto return T/F value; that's what they did.
 
 // Do not edit below this line
 module.exports = leapYears;
