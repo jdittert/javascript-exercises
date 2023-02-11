05b167981bcd4cb0b6deacc2c5d1e90e6eb92461
jdittert <jondittert@gmail.com>
2023-02-10 23:03:25 -0500

    Solve problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let finalSum = 0;
+    
+    if (typeof int1 === "number" && typeof int2 === "number") {
+        x = int1;
+        y = int2;
+    
+        if (x >=0 && y >=0) {
+            for (i = Math.min(x, y); i <= Math.max(x, y); i++) {
+                finalSum += i;
+            }
+        } else {
+            finalSum = "ERROR";
+        }
+    } else {
+        finalSum = "ERROR";
+    }  
+    
+    return finalSum
 };
 
 // Do not edit below this line
