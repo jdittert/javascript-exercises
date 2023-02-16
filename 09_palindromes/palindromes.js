81607c28606f69b5799d95e26b35c8fa0832c764
jdittert <jondittert@gmail.com>
2023-02-16 18:51:28 -0500

    Palindromes complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    let lowerString = words.toLowerCase();
+    const forString = lowerString.split("");
+    const forLetters = forString.filter(checkLetter);
+    function checkLetter(a) {
+        if (a >= "a" && a <= "z") {
+            return true;
+        };
+    }
+    const temp = [...forLetters];
+    const backLetters = forLetters.reverse();
+    return temp.join("") === backLetters.join("");
 };
 
 // Do not edit below this line
