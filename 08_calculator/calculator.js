13190c529707f0c5e46d1a43ca383d8f7517bb57
jdittert <jondittert@gmail.com>
2023-02-16 11:38:55 -0500

    Finish all exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  return arr.reduce((num, total) => num + total,
+  total);  
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  return arr.reduce((num, total) => num * total, total);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  } else {
+    let ans = num;
+    for (i = num; i > 1; i--) {
+      ans *= (i - 1);
+    };
+    return ans;
+  };
 };
 
 // Do not edit below this line
