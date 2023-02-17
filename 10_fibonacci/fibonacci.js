da80fd4669e3b5a28a9cb1165596597340873f03
jdittert <jondittert@gmail.com>
2023-02-17 10:41:53 -0500

    Fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(fib) {
+    let seq = [1, 1];
+    fib = +fib;
+    if (fib < 1) return "OOPS";
+    for (i = 2; i < +fib; i++) {
+        newFib = seq[i - 1] + seq[i - 2];
+        seq.push(newFib);
+    }
+    return seq[fib - 1];
 };
 
 // Do not edit below this line
